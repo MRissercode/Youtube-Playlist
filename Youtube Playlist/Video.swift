9e784f0497517de6564b9e434b765df6a4151e74
MRisser1 <student@MRisser1.local>
2017-04-12 11:27:36 -0500

    Created a custom class for videos.

---
+++ Youtube Playlist/Video.swift
@@ -0,0 +1,13 @@
+//
+//  Video.swift
+//  Youtube Playlist
+//
+//  Created by MRisser1 on 4/12/17.
+//  Copyright © 2017 MRisser1. All rights reserved.
+//
+
+import UIKit
+
+class Video: NSObject {
+
+}
